4a0522784f41ffe04c5d902db3924283f80c5d1a
Krzysiek <k.kotysz@gmail.com>
2020-03-17 21:39:10 +0100

    trzeci notebook

--- baza.bash
+++ baza.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
-start=$(head -n 2 TIC102090493-s2-121s_lc3_fppt.dat | tail -n 1)
+start=$(head -n 2 $1 | tail -n 1)
 start=$(echo $start | cut -d " " -f 1)
-end=$(tail -n 1 TIC102090493-s2-121s_lc3_fppt.dat)
+end=$(tail -n 1 $1)
 end=$(echo $end | cut -d " " -f 1)
 tbase=$(echo $end-$start | bc) # wyliczanie bazy czasowej
 echo $tbase
